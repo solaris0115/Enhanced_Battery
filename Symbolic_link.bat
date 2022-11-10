f1b1912b81e711bd50d8cfa47e6f67e1f4cdac00
solaris <solaris0115@gmail.com>
2022-11-11 00:48:20 +0900

    폴더 경로 수정

---
+++ Symbolic_link.bat
@@ -0,0 +1,3 @@
+rmdir "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\EnhancedBattery"
+mklink /d  "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\EnhancedBattery" D:\GitProject\EnhancedBattery\EnhancedBattery
+pause
